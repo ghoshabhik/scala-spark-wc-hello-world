fe9739858e20134c53722db3e04bbc15f801aa69
abhik <avik5324@gmail.com>
2020-07-27 00:43:51 +0530

    First Commit

---
+++ src/main/scala/com/github/MyApp.scala
@@ -0,0 +1,37 @@
+package com.github
+
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+
+object MyApp {
+  lazy val logger: Logger = Logger.getLogger(this.getClass)
+  val jobName = "MyApp"
+
+  def main(args: Array[String]): Unit = {
+    val sentences = Seq(
+      "Space.",
+      "The final frontier.",
+      "These are the voyages of the starship Enterprise.",
+      "Its continuing mission:",
+      "to explore strange new worlds,",
+      "to seek out new life and new civilizations,",
+      "to boldly go where no one has gone before!"
+    )
+
+    try {
+      val spark = SparkSession.builder().appName(jobName).master("local[*]").getOrCreate()
+      val rdd = spark.sparkContext.parallelize(sentences)
+      val counts = rdd.flatMap(splitSentenceIntoWords)
+        .map(word => (word, 1))
+        .reduceByKey(_ + _)
+        .sortBy(t => t._2, ascending = false)
+      counts.foreach(println)
+    } catch {
+      case e: Exception => logger.error(s"$jobName error in main", e)
+    }
+  }
+
+  def splitSentenceIntoWords(sentence: String): Array[String] = {
+    sentence.split(" ").map(word => word.toLowerCase.replaceAll("[^a-z]", ""))
+  }
+}
